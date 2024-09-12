0a9d5df34dd58ca16ffcc84ad6a6f99fa074ff7d
Ponce <ponceluis974@gmail.com>
2024-09-11 20:54:12 -0700

    fixed answers

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const questions = {
     'box-4a': 'Question for 100 in Category 4', 
     'box-1b': 'Question for 200 in Category 1',
     'box-2b': 'Question for 200 in Category 2', 
-    'box-3b': 'Question for 200 in Category 3', 
+    'box-3b': 'The term for the smallest unit of sound', 
     'box-4b': 'Question for 200 in Category 4', 
     'box-1c': 'Question for 300 in Category 1',
     'box-2c': 'Question for 300 in Category 2', 
@@ -22,44 +22,44 @@ const questions = {
 }; 
 const questionsV2 = {
     'box-1a': {
-        question: 'Question for 100 in Category 1',
-        correctAnswer: 'Correct Answer 1',
-        answers: ['Correct Answer 1', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
+        question: "the term for the practice of studying a culture from an outsider’s perspective and evaluating it based on one's own cultural standards?",
+        correctAnswer: 'Ethnocentrism',
+        answers: ['Cultural relativism', 'Ethnocentrism', 'Symbolic interactionsim', 'Cultural determinism']
     },
     'box-2a': {
-        question: 'Question for 100 in Category 2',
-        correctAnswer: 'Correct Answer 2',
-        answers: ['Correct Answer 2', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
+        question: 'Which part of the human skeleton is most crucial for upright walking',
+        correctAnswer: 'Pelvis',
+        answers: ['Arms', 'Pelvis', 'Toes', 'Skull']
     },
     'box-3a': {
-        question: 'Question for 100 in Category 3',
-        correctAnswer: 'Correct Answer 3',
-        answers: ['Correct Answer 3', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
+        question: 'A variant of a language spoken in a particular region or by a specific group',
+        correctAnswer: 'dialect',
+        answers: ['Code-switching', 'dialect', 'pidgin', 'creole']
     },
     'box-4a': {
-        question: 'Question for 100 in Category 4',
-        correctAnswer: 'Correct Answer 4',
-        answers: ['Correct Answer 4', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
+        question: 'This Archeologist fought nazis in the 1940s',
+        correctAnswer: 'Indiana Jones',
+        answers: ['Indiana Jones', 'Howard Carter', '', '']
     },
     'box-1b': {
-        question: 'Question for 200 in Category 1',
-        correctAnswer: 'Correct Answer 5',
-        answers: ['Correct Answer 5', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
+        question: 'what is a common term used to describe a kinship system in which individuals trace their ancestry through both maternal and paternal lines?',
+        correctAnswer: 'Bilateral descent',
+        answers: ['Bilateral descent', 'Unilineal descent', 'Matrilineal descent', 'Patrilineal descent']
     },
     'box-2b': {
-        question: 'Question for 200 in Category 2',
-        correctAnswer: 'Correct Answer 6',
-        answers: ['Correct Answer 6', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
+        question: 'Which of the following is considered one of the earliest known hominins',
+        correctAnswer: 'Australopithecus afarensis',
+        answers: ['Homo erectus', ' Homo sapiens', 'Neanderthals', 'Australopithecus afarensis']
     },
     'box-3b': {
-        question: 'Question for 200 in Category 3',
-        correctAnswer: 'Correct Answer 7',
+        question: 'The term for the smallest unit of sound',
+        correctAnswer: 'phoneme',
         answers: ['Correct Answer 7', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
     },
     'box-4b': {
-        question: 'Question for 200 in Category 4',
-        correctAnswer: 'Correct Answer 8',
-        answers: ['Correct Answer 8', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
+        question: 'mapping technology to document social patterns based on geological location',
+        correctAnswer: 'Geographic Imaging System',
+        answers: ['Geographic Imaging System', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
     },
     'box-1c': {
         question: 'Question for 300 in Category 1',
@@ -67,14 +67,14 @@ const questionsV2 = {
         answers: ['Correct Answer 9', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
     },
     'box-2c': {
-        question: 'Question for 300 in Category 2',
-        correctAnswer: 'Correct Answer 10',
-        answers: ['Correct Answer 10', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
+        question: 'Which of the following behaviors is commonly observed in primates and is crucial for social bonding',
+        correctAnswer: 'Grooming',
+        answers: ['Nest building', 'Tool use', 'Grooming', 'Vocalizations']
     },
     'box-3c': {
-        question: 'Question for 300 in Category 3',
-        correctAnswer: 'Correct Answer 11',
-        answers: ['Correct Answer 11', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
+        question: 'This North American language uses a cyclical explanation of time as opposed to linear',
+        correctAnswer: 'hopi language',
+        answers: ['hopi language', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
     },
     'box-4c': {
         question: 'Question for 300 in Category 4',
@@ -82,39 +82,39 @@ const questionsV2 = {
         answers: ['Correct Answer 12', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
     },
     'box-1d': {
-        question: 'Question for 400 in Category 1',
-        correctAnswer: 'Correct Answer 13',
-        answers: ['Correct Answer 13', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
+        question: 'a.png',
+        correctAnswer: 'Mayan',
+        answers: ['Mayan', 'Olmec', 'Babylonian', 'Egyptian']
     },
     'box-2d': {
-        question: 'Question for 400 in Category 2',
-        correctAnswer: 'Correct Answer 14',
-        answers: ['Correct Answer 14', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
+        question: 'What species was the famous skeleton Lucy',
+        correctAnswer: 'Australopithecus africanus',
+        answers: ['Australopithecus africanus', 'Homo sapiens', 'Homo floresiensis', 'Neanderthal']
     },
     'box-3d': {
-        question: 'Question for 400 in Category 3',
-        correctAnswer: 'Correct Answer 15',
-        answers: ['Correct Answer 15', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
+        question: 'Phonations occurs in this part of the body',
+        correctAnswer: 'The larynx/vocal folds',
+        answers: ['The larynx/vocal folds', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
     },
     'box-4d': {
-        question: 'Question for 400 in Category 4',
-        correctAnswer: 'Correct Answer 16',
-        answers: ['Correct Answer 16', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
+        question: 'Archaeologist re-create tools and building techniques to better understand past ways of life',
+        correctAnswer: 'experimental archaeology',
+        answers: ['experimental archaeology', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
     },
     'box-1e': {
-        question: 'Question for 500 in Category 1',
-        correctAnswer: 'Correct Answer 17',
-        answers: ['Correct Answer 17', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
+        question: 'This high demand religion believes that humans were created in a laboratory and planted on earth.',
+        correctAnswer: 'Raelian movement',
+        answers: ['Unification church', 'Raelian movement', 'Scientology', 'Branch Davidians']
     },
     'box-2e': {
-        question: 'Question for 500 in Category 2',
-        correctAnswer: 'Correct Answer 18',
-        answers: ['Correct Answer 18', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
+        question: 'Which of the following had a approximate human brain size of 500-600cc and lived around 2.4 to 1.4 million years ago?',
+        correctAnswer: 'Homo habilis',
+        answers: ['Homo habilis', 'Homo erectus', 'Australopithecus africanus', 'Homo sapiens']
     },
     'box-3e': {
-        question: 'Question for 500 in Category 3',
-        correctAnswer: 'Correct Answer 19',
-        answers: ['Correct Answer 19', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
+        question: 'this NAE phoneme is a glottal consonant',
+        correctAnswer: 'h',
+        answers: ['h', 'Incorrect Answer 1', 'Incorrect Answer 2', 'Incorrect Answer 3']
     },
     'box-4e': {
         question: 'Question for 500 in Category 4',
@@ -138,8 +138,12 @@ document.querySelectorAll('.box').forEach(box => {
         document.querySelectorAll('.bubble').forEach(bubble => {
             bubble.style.backgroundColor = ''; // Reset to default
         });
-
-        document.getElementById('popup-content').textContent = question
+        if (question === 'a.png') {
+            document.getElementById('popup-content').innerHTML = `<img src="${question}" alt="Question Image" style="max-width: 100%; height: auto;">`;
+        }else {
+            document.getElementById('popup-content').textContent = question
+        }
+        
         document.getElementById('answer-1').textContent = shuffledAnswers[0];
         document.getElementById('answer-2').textContent = shuffledAnswers[1];
         document.getElementById('answer-3').textContent = shuffledAnswers[2];
